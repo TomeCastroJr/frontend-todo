e2715c54a153bce21f7978016422996f8ef3e4ae
Jorge Tomé <jorge.tomejr@outlook.com>
2025-02-24 09:21:26 -0400

    feat: update TaskItem component

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import "./TaskItem.scss"
+
+import {AiFillDelete} from "react-icons/ai"
 
 const TaskItem = ({task}) => {
   return (
-    <div>
-        <p>{task.description}</p>
-        <p>Status: {task.isCompleted ? "Concluído" : "Pendente"}</p>
+    <div className='task-item-container'>
+      <div className='task-description'>
+        <label className={
+          task.isCompleted
+          ? "checkbox-container-completed"
+          : "checkbox-container"
+        }>
+          {task.description}
+          <input type="checkbox" defaultChecked={task.isCompleted}/>
+          <span className={task.isCompleted? "checkmark completed" : "checkmark"}></span>
+        </label>
+      </div>
+
+      <div className='delete'>
+        <AiFillDelete size={18} color="#f97474" />
+      </div>
     </div>
   )
 }
