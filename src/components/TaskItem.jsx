4eb257dc871cb5fac598bf4f24c6bef135efd13d
Jorge Tomé <jorge.tomejr@outlook.com>
2025-02-26 10:29:16 -0400

    add function to change a task status

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -20,12 +20,30 @@ const TaskItem = ({ task, getTasks}) => {
     }
   }
 
+  const handleTaskCompletedChange = async (e) =>{
+    try {
+      await axios.patch(`http://localhost:8000/tasks/${task._id}`, {
+        isCompleted: e.target.checked
+      });
+
+      await getTasks();
+
+      toast.success("A tarefa foi modificada com sucesso", {
+        position: 'bottom-right',
+      })
+
+    } catch (error) {
+      toast.error("Algo deu errado", {
+        position: "bottom-right"
+      })
+    }
+  }
   return (
     <div className='task-item-container'>
       <div className='task-description'>
         <label className={task.isCompleted? "checkbox-container-completed" : "checkbox-container"}>
           {task.description}
-          <input type="checkbox" defaultChecked={task.isCompleted}/>
+          <input type="checkbox" defaultChecked={task.isCompleted} onChange={e => handleTaskCompletedChange(e)}/>
           <span className={task.isCompleted? "checkmark completed" : "checkmark"}></span>
         </label>
       </div>
