e9bb693b5c0023bcf7f24e6aac259e03fe8e0ef7
Jorge Tomé <jorge.tomejr@outlook.com>
2025-02-28 12:08:30 -0400

    feat: add app container

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Tasks from "./components/Tasks";
 import { ToastContainer } from 'react-toastify';
+import SideBar from"./components/SideBar"
+
+import "./App.scss"
 
 function App() {
 
   return (
-    <>
+    <div className="app-container">
+      <SideBar/>
       <Tasks/>
       <ToastContainer/>
-    </>
+    </div>
   )
 }
 
